43ee002a03ad8976cbc3e9a6ad6c8bb14ed136f8
tylergreason <tyler.greason@protonmail.com>
2020-09-02 11:40:48 -0400

    Final commit

--- index.js
+++ index.js
@@ -1,4 +1,45 @@
-import { createEle } from "../modules/generalFunctions.js"
+import { createEle, newPara } from "./modules/generalFunctions.js"
 
-const myDiv = createEle('div', 'myDiv')
+const myDiv = createEle('div', 'myDiv');
+myDiv.style.width = '200px'; 
+myDiv.style.height = '20px'; 
+myDiv.style.backgroundColor = 'red'; 
+myDiv.innerText = "this div has been styled!"
+document.body.appendChild(myDiv);
 
+const newP = newPara('hey, this is text!', 'exampleClass');
+document.body.appendChild(newP);
+
+const newP2 = newPara('hi Medium!');
+document.body.appendChild(newP2);
+
+// import * as GeneralModule from "./modules/generalFunctions.js"
+
+// const myDiv = GeneralModule.createEle('div', 'myDiv');
+// myDiv.style.width = '200px'; 
+// myDiv.style.height = '20px'; 
+// myDiv.style.backgroundColor = 'red'; 
+// myDiv.innerText = "this div has been styled!"
+// document.body.appendChild(myDiv);
+
+// const newP = GeneralModule.newPara('hey, this is text!', 'exampleClass');
+// document.body.appendChild(newP);
+
+// const newP2 = GeneralModule.newPara('hi Medium!');
+// document.body.appendChild(newP2);
+
+
+// import { createEle as createEleRenamed, newPara as newParaAlsoRenamed} from "./modules/generalFunctions.js"
+
+// const myDiv = createEleRenamed('div', 'myDiv');
+// myDiv.style.width = '200px'; 
+// myDiv.style.height = '20px'; 
+// myDiv.style.backgroundColor = 'red'; 
+// myDiv.innerText = "this div has been styled!"
+// document.body.appendChild(myDiv);
+
+// const newP = newParaAlsoRenamed('hey, this is text!', 'exampleClass');
+// document.body.appendChild(newP);
+
+// const newP2 = newParaAlsoRenamed('hi Medium!');
+// document.body.appendChild(newP2);
--- modules/generalFunctions.js
+++ modules/generalFunctions.js
@@ -1,9 +1,14 @@
 export const createEle = (type, classes=null) => {
-    let newEle = document.createElement(type)
+    let newEle = document.createElement(type);
     // apply classes if there are any
     if (classes) {
-        newEle.classList.add(classes)
+        newEle.classList.add(classes);
     }
     return newEle; 
 }
 
+export const newPara = (text, classes=null) => {
+    const p = createEle('p', classes); 
+    p.innerText = text; 
+    return p; 
+}
